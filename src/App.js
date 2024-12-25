945fbd5dc7fc31aec7ec140ee7c9b74a03a4a768
akhilomeella <akhilomeella@gmail.com>
2024-12-25 21:53:47 +0100

    cart update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import AllStores from './pages/AllStores';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Productpage from './pages/Productpage';
-import ProductDetail from './component/ProductDetail';
-import Login from './component/Login';
-import SignUp from './component/Signup';
+import AllStores from "./pages/AllStores";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Productpage from "./pages/Productpage";
+import ProductDetail from "./component/ProductDetail";
+import Login from "./component/Login";
+import SignUp from "./component/Signup";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/all-stores" element={<AllStores />} />
           <Route path="/products" element={<Productpage />} />
-          <Route path="/detail" element={<ProductDetail />}  />
+          <Route path="/detail" element={<ProductDetail />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </div>
     </Router>
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,47 +1,78 @@
-import React from 'react'
-import { useNavigate} from 'react-router-dom'
-import Search from '../assets/Component 2.png'
-import Cart from '../assets/Cart1.png'
-import User from '../assets/user.png'
-import Logo from '../assets/siyulogo.svg'
+import React from "react";
+import { useNavigate, Link } from "react-router-dom";
+import Search from "../assets/Component 2.png";
+import Cart from "../assets/Cart1.png";
+import User from "../assets/user.png";
+import Logo from "../assets/siyulogo.svg";
 
 function Navbar() {
-    const navigate = useNavigate()
-
+  const navigate = useNavigate();
 
   return (
-    <div className='w-full flex items-center justify-between mt-8 '>
-        <div className='flex items-end justify-center' onClick={() => navigate('/')}>
-            <img src={Logo} className='h-[46px] w-[60px]' alt="Siyu Market" /><h2 className='text-3xl font-semibold'>Siyu Market</h2>
+    <div className="w-full flex items-center justify-between mt-8 ">
+      <div
+        className="flex items-end justify-center"
+        onClick={() => navigate("/")}
+      >
+        <img src={Logo} className="h-[46px] w-[60px]" alt="Siyu Market" />
+        <h2 className="text-3xl font-semibold">Siyu Market</h2>
+      </div>
+      <div className="flex items-center">
+        <div
+          className="text-normal mr-[24px] cursor-pointer"
+          onClick={() => navigate("/")}
+        >
+          Home
         </div>
-        <div className='flex items-center'>
-            <div className='text-normal mr-[24px] cursor-pointer' onClick={() => navigate('/')}>Home</div>
-            <div className='text-normal mr-[24px] cursor-pointer' onClick={() => navigate('/all-stores')}>Vendors</div>
-            <div className='text-normal mr-[24px] cursor-pointer' onClick={() => navigate('/products')}>Products</div>
-            <div className='text-normal mr-[24px] cursor-pointer' onClick={() => navigate('/')}>About Us</div>
-            
+        <div
+          className="text-normal mr-[24px] cursor-pointer"
+          onClick={() => navigate("/all-stores")}
+        >
+          Vendors
         </div>
-        <div className='flex items-center justify-center'>
-            <div className='bg-gray-200 rounded-sm flex w-[243px] h-[38px] mr-[16px] items-center justify-center outline-none border-0'>
-                <input type="text" 
-                    placeholder='Search' 
-                    className='bg-transparent' 
-                />
-                <img src={Search} alt="Search Icon" className='my-[7px] cursor-pointer' />
-                
-            </div>
-            <div className='flex justify-between items-center'>
-                <div>
-                    <img src={Cart} alt="Cart Icon" className='my-[7px] mr-[16px] cursor-pointer ' />
-                </div>
-                <div>
-                    <img src={User} alt="Search Icon" className='my-[7px] cursor-pointer ' />
-                </div>
-            </div>
+        <div
+          className="text-normal mr-[24px] cursor-pointer"
+          onClick={() => navigate("/products")}
+        >
+          Products
         </div>
-      
+        <div
+          className="text-normal mr-[24px] cursor-pointer"
+          onClick={() => navigate("/")}
+        >
+          About Us
+        </div>
+      </div>
+      <div className="flex items-center justify-center">
+        <div className="bg-gray-200 rounded-sm flex w-[243px] h-[38px] mr-[16px] items-center justify-center outline-none border-0">
+          <input type="text" placeholder="Search" className="bg-transparent" />
+          <img
+            src={Search}
+            alt="Search Icon"
+            className="my-[7px] cursor-pointer"
+          />
+        </div>
+        <div className="flex justify-between items-center">
+          <div>
+            <Link to="/cart">
+              <img
+                src={Cart}
+                alt="Cart Icon"
+                className="my-[7px] mr-[16px] cursor-pointer "
+              />
+            </Link>
+          </div>
+          <div>
+            <img
+              src={User}
+              alt="Search Icon"
+              className="my-[7px] cursor-pointer "
+            />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
